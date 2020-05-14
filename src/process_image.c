47ae141b7522ca625ba366703e70ef29d8825511
leechannl <leechannl@gmail.com>
2020-05-14 15:06:37 +0800

    add rgb_to_hsv, hsv_to_rgb

--- src/process_image.c
+++ src/process_image.c
@@ -3,37 +3,24 @@
 #include <assert.h>
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 float get_pixel(image im, int x, int y, int c) {
   // clamp padding strategy
-  if (x > im.w) {
-    x = im.w;
-  }
-  if (x < 0) {
-    x = 0;
-  }
-  if (y > im.h) {
-    y = im.h;
-  }
-  if (y < 0) {
-    y = 0;
-  }
-  if (c > im.c) {
-    c = im.c;
-  }
-  if (c < 0) {
-    c = 0;
-  }
+  if (x > im.w) { x = im.w - 1; }
+  if (x < 0) { x = 0; }
+  if (y > im.h) { y = im.h - 1; }
+  if (y < 0) { y = 0; }
+  if (c > im.c) { c = im.c - 1; }
+  if (c < 0) { c = 0; }
 
   return im.data[x + im.w * y + im.w * im.h * c];
 }
 
 void set_pixel(image im, int x, int y, int c, float v) {
   // checking bounds
-  if (x > im.w || x < 0 || y > im.h || y < 0 || c > im.c || c < 0) {
-    return;
-  }
+  if (x > im.w || x < 0 || y > im.h || y < 0 || c > im.c || c < 0) { return; }
 
   im.data[x + im.w * y + im.w * im.h * c] = v;
 }
@@ -79,7 +66,7 @@ image rgb_to_grayscale(image im) {
       //           get_pixel(im, i, j, 2));
       float y = 0.299 * get_pixel(im, i, j, 0) +
                 0.587 * get_pixel(im, i, j, 1) + 0.114 * get_pixel(im, i, j, 2);
-      gray.data[i + j * im.w] = y;
+      set_pixel(gray, i, j, 0, y);
     }
   }
 
@@ -98,12 +85,8 @@ void clamp_image(image im) {
   for (int i = 0; i < im.w; i++) {
     for (int j = 0; j < im.h; j++) {
       for (int k = 0; k < im.c; k++) {
-        if (get_pixel(im, i, j, k) > 1) {
-          set_pixel(im, i, j, k, 1);
-        }
-        if (get_pixel(im, i, j, k) < 0) {
-          set_pixel(im, i, j, k, 0);
-        }
+        if (get_pixel(im, i, j, k) > 1) { set_pixel(im, i, j, k, 1); }
+        if (get_pixel(im, i, j, k) < 0) { set_pixel(im, i, j, k, 0); }
       }
     }
   }
@@ -119,9 +102,73 @@ float three_way_min(float a, float b, float c) {
 }
 
 void rgb_to_hsv(image im) {
-  // TODO Fill this in
+  for (int y = 0; y < im.h; y++) {
+    for (int x = 0; x < im.w; x++) {
+      float r = get_pixel(im, x, y, 0);
+      float g = get_pixel(im, x, y, 1);
+      float b = get_pixel(im, x, y, 2);
+
+      float v = three_way_max(r, g, b);
+      float m = three_way_min(r, g, b);
+
+      float c = v - m;
+      float s = 0;
+
+      if (v > 0) s = c / v;
+
+      float h_ = 0;
+      if (c != 0) {
+        if (v == r) h_ = (g - b) / c;
+        if (v == g) h_ = (b - r) / c + 2;
+        if (v == b) h_ = (r - g) / c + 4;
+      }
+      float h = h_ / 6;
+      if (h_ < 0) h = h_ / 6 + 1;
+
+      set_pixel(im, x, y, 0, h);
+      set_pixel(im, x, y, 1, s);
+      set_pixel(im, x, y, 2, v);
+    }
+  }
 }
 
 void hsv_to_rgb(image im) {
-  // TODO Fill this in
+  for (int i = 0; i < im.w; i++) {
+    for (int j = 0; j < im.h; j++) {
+      float h = get_pixel(im, i, j, 0) * 360;
+      float s = get_pixel(im, i, j, 1);
+      float v = get_pixel(im, i, j, 2);
+
+      float r = 0;
+      float g = 0;
+      float b = 0;
+      float c = 0;
+      float m = 0;
+      float x = 0;
+
+      c = s * v;
+      x = c * (1 - fabs(fmod(h / 60, 2) - 1));
+      m = v - c;
+      if (h < 60) {
+        r = c, g = x, b = 0;
+      } else if (h < 120) {
+        r = x, g = c, b = 0;
+      } else if (h < 180) {
+        r = 0, g = c, b = x;
+      } else if (h < 240) {
+        r = 0, g = x, b = c;
+      } else if (h < 300) {
+        r = x, g = 0, b = c;
+      } else if (h < 360) {
+        r = c, g = 0, b = x;
+      } else {
+        log_error("hue overflow!");
+        exit(1);
+      }
+
+      set_pixel(im, i, j, 0, r + m);
+      set_pixel(im, i, j, 1, g + m);
+      set_pixel(im, i, j, 2, b + m);
+    }
+  }
 }
